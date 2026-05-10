9df9e89e7928bb712b117d7e8136391e069ae164
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-10 18:01:20 +0300

    -Theme ekraninda primary olusturldu,bu feed ve favorites ekranlarinda bulunan ikon ve slogan yazisinin rengi için olusturuldu. - Logo için header olusturuldu.

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,9 +1,12 @@
 export const COLORS = {
+    header: '#280306',      // Logo ve Frame alanı (Bordo)
+    black: '#000000',
     background: '#CCC7CD',
     cardBackground: '#6F123F',
     white: '#FFFFFF',
     textLight: '#F5F5F5',
     soldOut: '#555555',
+    primary: '#000000',
 };
 
 export const SIZES = {
